cb64d9ccf9b037747d15f7bc6ede10cbe969eff4
prachi84 <prachi8384@gmail.com>
2021-08-23 16:29:59 -0700

    login functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,30 @@
 import React, {Component} from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import { Provider } from 'react-redux';
+import jwt_decode from 'jwt-decode';
 import './App.css';
 import Footer from './components/layout/Footer';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+import setAuthToken from './utils/setAuthToken';
+import { SET_CURRENT_USER } from './actions/types';
 import store from './store';
+
+if (localStorage.jwtToken){
+  //Set token to auth header
+  setAuthToken(localStorage.jwtToken);
+
+  //Decode token
+  const decoded = jwt_decode(localStorage.jwtToken);
+  store.dispatch({
+    type: SET_CURRENT_USER,
+    payload: decoded
+  });
+
+  
+}
 
 class App extends Component {
 
